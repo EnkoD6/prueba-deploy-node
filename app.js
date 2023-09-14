c1e0954ab522ef387c6a33562ee29e7620b22eb0
Eneko Olabarria <enekolabarria@gmail.com>
2023-09-14 13:32:10 +0200

    commit inicial para el deploy

---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const movies = require('./movies.json')
+const crypto = require('node:crypto')
+const validateMovie = require('./movies')
+const validatePartialMovie = require('./movies')
+
+const app = express()
+app.disable('x-powered-by')
+
+app.get('/movies', (req, res) => {
+  res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => { // path-to-regexp
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+
+  res.status(404).json({ message: 'Movie not found' })
+})
+
+app.get('/movies', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+        movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
+  res.json(movies)
+})
+
+app.post('/movies', (req, res) => {
+ 
+    const result = validateMovie(req.body)
+
+    if (result.error) {
+        return res.status(400).json({ error: result.error.message })
+    }
+
+    const newMovie = {
+        id: crypto.randomUUID(), // uuid v4
+        ...result.data
+    }
+
+    // Esto no sería REST, porque se esta guardando el estado de la app en memoria
+    movies.push(newMovie)
+
+    res.status(201).json(newMovie)
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+  
+    if (!result.success) {
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+    const { id } = req.params
+    const movieIndex = movies.findIndex(m => m.id === id)
+
+    if (movieIndex === -1) {
+        return res.status(404).json({ message: 'Movie not found' })
+    }
+
+    const updateMovie = {
+        ...movies[movieIndex],
+        ...result.data
+    }
+
+    return res.json(updateMovie)
+})
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+    console.log(`server listening on port http://localhost:${PORT}`)
+})
